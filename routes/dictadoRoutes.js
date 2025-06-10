e2c720d10180da7b2226645d1cdc1831a40d37b9
Agustin Fraygola <agustinfraygola2001@gmail.com>
2025-06-10 18:26:08 -0300

    update+delete dictado endpoints draft

--- routes/dictadoRoutes.js
+++ routes/dictadoRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const Clase = require("../models/Clase");
 const Dictado = require("../models/Dictado");
-const router = express.Router();
+const { default: mongoose } = require("mongoose");
 
-router.get("/clases/:id", async (req, res) => {
+const app = express();
+app.use(express.json());
+
+const getDictado = async (req, res) => {
     try {
         const idClase = req.params.id;
 
@@ -40,19 +43,29 @@ router.get("/clases/:id", async (req, res) => {
             message: "ID inválido o error en la solicitud",
         });
     }
-})
+};
 
-/*
-POST REQUEST
--que validaciones necesito
-    + los campos obligatorios (+ agregar defaults y campos obligatorios en dictado schema)
-    + array listado??
-    + formato de fecha?
--como se guarda el dictado
--que devuelvo como respuesta
-*/
+const getTodosDictados = async (req, res) => {
+  try {
+    const dictados = await Dictado.find();
 
-router.post("/clase/:id", async (req, res) => {
+    res.status(200).json({
+      status: "success",
+      results: dictados.length,
+      data: {
+        dictados,
+      },
+    });
+  } catch (error) {
+    console.error("Error al obtener dictados:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Error interno al obtener los dictados",
+    });
+  }
+}
+
+const postDictado = async (req, res) => {
     /*
     datos que necesito
     profesor
@@ -101,9 +114,93 @@ router.post("/clase/:id", async (req, res) => {
             message: "No se pudo guardar el dictado en la base de datos"
         });
     }
-})
+}
+
+const patchDictado = async (req, res) => {
+    const id = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            status: "fail",
+            message: "ID invalido"
+        })
+    }
+
+    try {
+        const datosActualizados = req.body;
+
+        const dictado = await Dictado.findByIdAndUpdate(id, datosActualizados, {
+            new: true,
+            runValidators: true,
+        })
+
+        if (!dictado) {
+            return res.status(404).json({
+                status: "fail",
+                message: "No se encontro dictado con ID"
+            })
+        }
+
+        return res.status(200).json({
+            status: "success",
+            data: {
+                dictado,
+
+            },
+        });
+    } catch (error) {
+        console.error("Error al actualizar el dictado:", error);
+        return res.status(500).json({
+            status: "error",
+            message: "Error interno del servidor al actualizad el dictado",
+        })
+    }
+}
+
+const deleteDictado = async (req, res) => {
+
+    const idDictado = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(idDictado)) {
+        return res.status(400).json({
+            status: "fail",
+            message: "ID invalido"
+        })
+    }
+
+    try{
+        const dictadoEliminado = await Dictado.findByIdAndDelete(id);
+
+        if (!dictadoEliminado) {
+            return res.status(404).json({
+                status: "fail",
+                message: "No se encontro un dictado con el ID"
+            })
+        }
+
+        res.status(204).json({
+            status: "success",
+            data: null
+        })
+    }catch(error){
+        console.error("Error al actualizar el dictado:", error);
+        return res.status(500).json({
+            status: "error",
+            message: "Error interno del servidor al actualizad el dictado",
+        })
+    }
+}
+
+app
+    .get("/clases/:id", getDictado)
+    .get("/clase", getTodosDictados)
+    .post("/clase/:id", postDictado)
+    .patch("/clase/:id", patchDictado)
+    .delete("/clase/:id", deleteDictado)
+
+/*
+    falta agregar mas validaciones
+*/
 
 
-
-
-module.exports = router;
+module.exports = app;
